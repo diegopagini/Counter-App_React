4e4ef1884ee3704041f2fc5ab0c67e69b5770ed7
Diego Pagini <dq@qbkconsulting.com>
2021-04-04 09:07:46 -0300

    tests

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,3 +1,9 @@
 /** @format */
 
-import '@testing-library/jest-dom/extend-expect';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { createSerializer } from 'enzyme-to-json';
+// import '@testing-library/jest-dom/extend-expect';
+
+Enzyme.configure({ adapter: new Adapter() });
+expect.addSnapshotSerializer(createSerializer({ mode: 'deep' }));
